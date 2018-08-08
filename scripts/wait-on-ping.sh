5e69bf3560aaae8dfef532f69d42e51d009be037
blackholegalaxy <blackholegalaxy@users.noreply.github.com>
2018-08-08 12:40:35 -0400

    feat: add wait for ping script

---
+++ scripts/wait-on-ping.sh
@@ -0,0 +1,13 @@
+attempt_counter=0
+max_attempts=20
+
+until $(ping -c 1 "$1"); do
+    if [ ${attempt_counter} -eq ${max_attempts} ];then
+      echo "Max attempts reached"
+      exit 1
+    fi
+
+    printf '.'
+    attempt_counter=$(($attempt_counter+1))
+    sleep 5
+done
